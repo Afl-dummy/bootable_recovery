AxionAOSP/android_bootable_recovery

overriding LineageOS properties
Change-Id: I791fabee11af97785edd0f22d59849d4ee4cdc82

recovery.cpp
@@ -879,13 +879,13 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   // Extract the YYYYMMDD / YYYYMMDD_HHMMSS timestamp from the full version string.
   // Assume the first instance of "-[0-9]{8}-", or "-[0-9]{8}_[0-9]{6}-" in case
   // LINEAGE_VERSION_APPEND_TIME_OF_DAY is set to true has the desired date.
-  std::string ver = android::base::GetProperty("ro.lineage.version", "");
+  std::string ver = android::base::GetProperty("ro.axion.version", "");
   std::smatch ver_date_match;
   std::regex_search(ver, ver_date_match, std::regex("-(\\d{8}(_\\d{6})?)-"));
   std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.lineage.build.version", "(unknown)") +
+    "Version " + android::base::GetProperty("ro.axion.build.version", "(unknown)") +
         " (" + ver_date + ")",
   };
   title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));

Afl-dummy/bootable_recovery

Signed-off-by: HinohArata <mmgcntk@gmail.com>

recovery.cpp
@@ -879,13 +879,13 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   // Extract the YYYYMMDD / YYYYMMDD_HHMMSS timestamp from the full version string.
   // Assume the first instance of "-[0-9]{8}-", or "-[0-9]{8}_[0-9]{6}-" in case
   // LINEAGE_VERSION_APPEND_TIME_OF_DAY is set to true has the desired date.
-  std::string ver = android::base::GetProperty("ro.lineage.version", "");
+  std::string ver = android::base::GetProperty("ro.afterlife.version", "");
   std::smatch ver_date_match;
   std::regex_search(ver, ver_date_match, std::regex("-(\\d{8}(_\\d{6})?)-"));
   std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.lineage.build.version", "(unknown)") +
+    "Version " + android::base::GetProperty("ro.afterlife.build.version", "(unknown)") +
         " (" + ver_date + ")",
   };
   title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));